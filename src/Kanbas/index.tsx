a085659fd7ee0461a344a45ab7776403bde09d31
leo.senai <LSenai@gmail.com>
2024-04-04 23:40:42 -0400

    debugging deployed instance

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -14,7 +14,7 @@ const API_BASE = process.env.REACT_APP_API_BASE;
 function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]); 
   // const COURSES_API = "http://localhost:4000/api/courses";
-  const COURSES_API = `${API_BASE}/api/courses`;
+  const COURSES_API = `https://kanbas-node-server-pyju.onrender.com/api/courses`;
 
   const findAllCourses = async () => {
     const response = await axios.get(COURSES_API);
